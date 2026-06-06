800d8581ba8cc34386ea5f208a2483308e181e24
LAPTOP-8KNL1P5V\Pozol <pozolero2005@gmail.com>
2026-06-06 17:33:41 -0600

    feat: reperacion de variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    production: true,
+  // Estas son las variables de Vercel que configuraste
+  supabaseUrl: 'https://clnoejuzybfxmpbzjrsm.supabase.com' , 
+  supabaseKey: 'sb_publishable_zajrhlJCIbZq5NsBeqM93g_wQe6UaU3'
+};
