c9c061493693e1abfc9f1cd164a61d091470feb3
LAPTOP-8KNL1P5V\Pozol <pozolero2005@gmail.com>
2026-06-06 17:45:47 -0600

    feat: otra reparacion

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://clnoejuzybfxmpbzjrsm.supabase.com',
+  supabaseUrl: 'https://clnoejuzybfxmpbzjrsm.supabase.co',
   supabaseKey: 'sb_publishable_zajrhlJCIbZq5NsBeqM93g_wQe6UaU3',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
   // Estas son las variables de Vercel que configuraste
-  supabaseUrl: 'https://clnoejuzybfxmpbzjrsm.supabase.com' , 
+  supabaseUrl: 'https://clnoejuzybfxmpbzjrsm.supabase.co' , 
   supabaseKey: 'sb_publishable_zajrhlJCIbZq5NsBeqM93g_wQe6UaU3'
 };
