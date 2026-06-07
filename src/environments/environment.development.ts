432deae1a19821081330a8321c2d6944598bfb11
LAPTOP-8KNL1P5V\Pozol <pozolero2005@gmail.com>
2026-06-06 18:22:48 -0600

    feat: la ptm ya me canse esta es la ultima

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://clnoejuzybfxmpbzjrsm.supabase.co/rest/v1/',
+  supabaseUrl: 'https://clnoejuzybfxmpbzjrsm.supabase.co',
   supabaseKey: 'sb_publishable_zajrhlJCIbZq5NsBeqM93g_wQe6UaU3',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
   // Estas son las variables de Vercel que configuraste
-  supabaseUrl: 'https://clnoejuzybfxmpbzjrsm.supabase.co/rest/v1/' , 
+  supabaseUrl: 'https://clnoejuzybfxmpbzjrsm.supabase.co' , 
   supabaseKey: 'sb_publishable_zajrhlJCIbZq5NsBeqM93g_wQe6UaU3'
 };
